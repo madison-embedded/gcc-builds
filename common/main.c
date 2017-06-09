59741427ad96fa4dd9d77b8cdf6c7f68f92d6cd2
vkottler <vkottler@wisc.edu>
2017-06-08 23:51:26 -0500

    malloc is just returning 0

--- common/main.c
+++ common/main.c
@@ -8,13 +8,30 @@ void print_post_info(void) {
 int main(void) {
 
 	unsigned int curr = 0, prev = 0;
+	void *malloc_test;
 
-	if (!board_init()) fault();
+	if (!board_init())
+		fault();
 
 	print_post_info();
 
+	malloc_test = malloc(4);
+
+	/* TODO: get this to not happen */
+	/* at least it works at all! */
+	if (!malloc_test) {
+		while(1) {
+			curr = ticks / 1000;
+			if (curr != prev)
+				(curr % 2) ? 
+					setGreen(true) : setGreen(false);
+			prev = curr;
+		}
+	}
+
 	while(1) {
 
+		/* Handle Button */
 		if (readButton()) {
 			setBlue(true);
 			setGreen(true);
@@ -26,12 +43,11 @@ int main(void) {
 
 		//processCommand();
 
+		/* Blink Red LED */
 		curr = ticks / 1000;
-
-		if (curr != prev) {
-			(curr % 2) ? setRed(true) : setRed(false);
-		}
-
+		if (curr != prev)
+			(curr % 2) ? 
+				setRed(true) : setRed(false);
 		prev = curr;
 	}
 }
