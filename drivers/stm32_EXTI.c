79c1f12b6f6c10e918a85fff10f1f97211397035
Cooper Green <csgreen3@wisc.edu>
2017-06-20 22:33:41 -0500

    set up interrupt clearn and tested hard coded interrupt handler

--- drivers/stm32_EXTI.c
+++ drivers/stm32_EXTI.c
@@ -1,50 +1,96 @@
+int count;
 
-
-static IRQn_Type exti_get_irq_num(EXTI_TypeDef* exti) {
-   switch((uint32_t) exti) {
-     case USART1_BASE: return EXTI0_IRQn;
-     case USART2_BASE: return EXTI1_IRQn;
-     case USART3_BASE: return EXTI2_IRQn;
-     case UART4_BASE:  return EXTI3_IRQn;
-     case UART5_BASE:  return EXTI4_IRQn;
-     case USART6_BASE: return EXTI9_5_IRQn;
-     case USART6_BASE: return EXTI15_10_IRQn;
-     default: return SysTick_IRQn;	/* -1 */
+static IRQn_Type exti_get_irq_num(uint32_t pin) {
+   switch(pin) {
+     case 0: return EXTI0_IRQn;
+     case 1: return EXTI1_IRQn;
+     case 2: return EXTI2_IRQn;
+     case 3:  return EXTI3_IRQn;
+     case 4:  return EXTI4_IRQn;
+     case 5 ... 9: return EXTI9_5_IRQn;
+     case 10 ... 15: return EXTI15_10_IRQn;
+     default: return 0xffff;	/* -1 */
    }
 }
 
-int exti_config(EXTI_TypeDef* exti, UINT32 pin  bool ie) {
-    exti->IMR /* interrupt mask register */
-    exti->EMR /* event mask register */
-    exti->RTSR /* rising trigger selection register */
-    exti->FTSR /*falling trigger selection register */
-    exti->SWIER /* software interrupt event register */
-    exti->PR /* pending register */
+int exti_config(uint32_t pin,  bool ie) {
+    count= 0;
 
+    EXTI->IMR = EXTI_IMR_MR3_Msk; /* interrupt mask register */
+    EXTI->RTSR = EXTI_RTSR_TR3_Pos; /* rising trigger selection register */
+    EXTI->FTSR = EXTI_FTSR_TR3_Msk;/*falling trigger selection register */
+  
+    SYSCFG->EXTICR[0] = SYSCFG_EXTICR1_EXTI3_PA; 	/* SYSCFG external interrupt configuration registers */
+    
     if (ie) {
-		NVIC_SetPriority(exti_get_irq_num(usart), 4);
-		NVIC_EnableIRQ(exti_get_irq_num(usart));
+		NVIC_SetPriority(exti_get_irq_num(pin), 4);
+		NVIC_EnableIRQ(exti_get_irq_num(pin));
 	}
 }
 
 
+
+void getNumInter(void){
+
+printf("number of interrupts = %d\n", count);
+}
+
+
+
+
+
 void EXTI0_IRQHandler(void){
+    printf("interrupted from pin 1 port A\n");
+    count ++;
+    EXTI->PR=EXTI_PR_PR0;
 }
 
 void EXTI1_IRQHandler(void){
+    EXTI->PR=EXTI_PR_PR1;
+
 }
 
+
 void EXTI2_IRQHandler(void){
+    EXTI->PR=EXTI_PR_PR2;
+
 }
 
 void EXTI3_IRQHandler(void){
+    count ++;
+    EXTI->PR=EXTI_PR_PR3;
+
 }
 
 void EXTI4_IRQHandler(void){
+    EXTI->PR=EXTI_PR_PR4;
 }
 
 void EXTI9_5_IRQHandler(void){
+    if (EXTI->PR & EX TI_PR_PR5)
+        EXTI->PR=EXTI_PR_PR5;
+    if (EXTI->PR & EX TI_PR_PR6)
+        EXTI->PR=EXTI_PR_PR6;
+    if (EXTI->PR & EX TI_PR_PR7)
+        EXTI->PR=EXTI_PR_PR7;
+    if (EXTI->PR & EX TI_PR_PR8)
+        EXTI->PR=EXTI_PR_PR8;
+    if (EXTI->PR & EX TI_PR_PR9)
+        EXTI->PR=EXTI_PR_PR9;
+
 }
 
 void EXTI15_10_IRQHandler(void){
+   if (EXTI->PR & EX TI_PR_PR10)
+        EXTI->PR=EXTI_PR_PR10;
+    if (EXTI->PR & EX TI_PR_PR11)
+        EXTI->PR=EXTI_PR_PR11;
+    if (EXTI->PR & EX TI_PR_PR12)
+        EXTI->PR=EXTI_PR_PR12;
+    if (EXTI->PR & EX TI_PR_PR13)
+        EXTI->PR=EXTI_PR_PR13;
+    if (EXTI->PR & EX TI_PR_PR14)
+        EXTI->PR=EXTI_PR_PR14;
+    if (EXTI->PR & EX TI_PR_PR15)
+        EXTI->PR=EXTI_PR_PR15;
 }
