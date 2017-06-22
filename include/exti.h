3e9ae1e7681314210a1ff7a36a01dbd57a1e7797
Cooper Green <csgreen3@wisc.edu>
2017-06-21 22:30:21 -0500

    updated exti.h to inculde ifdef

--- include/exti.h
+++ include/exti.h
@@ -1,4 +1,7 @@
+#ifndef _exti__H__
+#define _exti__H__
 
-
-int exti_config(uint32_t pin, char port, bool ie);
+int exti_config(uint32_t pin, char port, bool rtsr, bool ftsr, bool ie);
 void getNumInter(int pin);
+
+#endif
