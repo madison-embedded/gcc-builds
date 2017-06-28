b360eadd8a465d8339444d627e80dc0dd1bb0df7
davidguyuchen <davidguyuchen@gmail.com>
2017-06-28 18:25:34 -0500

    Implemented (needs input handling)

--- common/cmd_mem.c
+++ common/cmd_mem.c
@@ -1,36 +1,65 @@
 #include <stdio.h>
 #include "cli.h"
 #include <string.h>
-
+#include <stdlib.h>
 
 command_status do_md(int argc, char *argv[]) {
 	//printf("md not implemented\r\n");
 
 	//Todo: input checking
 
-	unsigned char* mem_ptr = (unsigned char*) atoi(argv[1]);
-	int length = atoi(argv[2]);
-	char* ascii_content = (char*) malloc(16*sizeof(char));
-	char content;
-	int i; //loop count
+	int i;
+	char* address = (char*) malloc( 9 * sizeof(char)); 
+   
+	for(i = 0; i < 8; i++)
+		address[i] = argv[1][i+2];
+	
+    address[8] = '\0';
 
+	unsigned char* mem_ptr = (unsigned char *) strtol(address, NULL, 16);
+    
+	int length = atoi(argv[2]);
+	char* ascii_content = (char*) malloc(17*sizeof(char));
+	ascii_content[16] = '\0';
+	char content;
+	int temp;
+      
 	for(i=0; i<length; i++)
 	{
 		content = (char) mem_ptr[i];
  
 		if(i % 16 == 0) //new line
 		{
-			printf(" %s\n", ascii_content);
-			printf("%x:", &mem_ptr[i]);
-		}
+ 		
+			printf("%8p:", &mem_ptr[i]);
+		};
 		if(i % 4 == 0) //space
 		{
 			printf(" ");
 		}
-		printf("%x", content);
+		
+		if(content >= 16)
+			printf("%x", content);
+		else 
+			printf("0%x", content);
 		ascii_content[i % 16] = (content >= 0x20 && content <= 0x7e) ? content : '.' ;
-	}
 	
+		if(i%16 == 15) printf(" %s\r\n", ascii_content);
+	}
+
+	if(length % 16 != 0) 
+	{	
+		temp = 16 - length%16;
+		
+		for(i = 0; i < temp; i++)
+			ascii_content[length%16 + i] = ' ';
+
+		temp = 2*temp + temp / 4;
+		for(i = 0; i < temp; i++ )
+			printf(" ");
+
+		printf(" %s\r\n", ascii_content);
+	}
 	return USAGE;
 }
 COMMAND_ENTRY("md", "md <addr> <count>", "View raw memory contents.", do_md)
@@ -41,3 +70,12 @@ command_status do_mw(int argc, char *argv[]) {
 }
 COMMAND_ENTRY("mw", "mw <addr> <value> <count>", "Write data memory contents.", do_mw)
 
+
+
+command_status do_memmap(int argc, char *argv[]) {
+	printf("HEEEY\r\n");
+	return USAGE;
+}
+
+
+COMMAND_ENTRY("memmap", "mw <addr> <value> <count>", "Memory map", do_memmap)
