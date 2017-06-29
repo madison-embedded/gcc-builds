f4ac5d3f5ef4643e27582c5d0e813a445c2deb1e
davidguyuchen <davidguyuchen@gmail.com>
2017-06-28 19:30:14 -0500

    Need to add more peripherals and make a better formatting

--- common/cmd_mem.c
+++ common/cmd_mem.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "proc/defs.h" 
+#include "cli.h"
 
 
 
@@ -23,7 +24,7 @@ command_status do_memmap(int argc, char*argv[]) {
 	printf("Physical     		 \t Target \t Size \r\n");
  
 	/* AHB1 Start */
-g
+
 	printf("USB OTG HS   		 \t %x \t %d bytes \r\n",USB_OTG_HS,  0x4007ffff - (int)USB_OTG_HS);
 	printf("DMA2D        		 \t %x \t %d bytes \r\n",DMA2D_BASE,       0x4002BBFF - DMA2D_BASE);
 	//printf("Ethernet MAC \t %x \t %d bytes \n", 
@@ -33,7 +34,7 @@ g
 	printf("Flash interface register \t %x \t %d bytes \r\n",FLASH_R_BASE,0x40023FFF - FLASH_R_BASE); 
 	printf("RCC			 \t %x \t %d bytes \r\n",RCC_BASE	   ,0x40023BFF - RCC_BASE);
 	printf("CRC                      \t %x \t %d bytes \r\n",CRC_BASE        ,0x400233FF - CRC_BASE);
-	printf("GPIOK                    \t %x \t %d bytes \r\n",GPIOK_BASE      ,0x40022800  - GPIOK_BASE);
+	printf("GPIOK                    \t %x \t %d bytes \r\n",GPIOK_BASE      ,0x40022BFF  - GPIOK_BASE);
 	printf("GPIOJ                    \t %x \t %d bytes \r\n",GPIOJ_BASE      ,0x400227FF  - GPIOJ_BASE);
 	printf("GPIOI                    \t %x \t %d bytes \r\n",GPIOI_BASE      ,0x400223FF  - GPIOI_BASE);
 	printf("GPIOH                    \t %x \t %d bytes \r\n",GPIOH_BASE      ,0x40021FFF  - GPIOH_BASE); 
@@ -48,14 +49,14 @@ g
 	/* END */
 	
 	
-	printf("MDIOS                    \t %x \t %d bytes \r\n",MDIOS_BASE      ,0x400103FF  - MDIOS_BASE); 
+	printf("MDIOS                    \t %x \t %d bytes \r\n",MDIOS_BASE      ,0x40017BFF  - MDIOS_BASE); 
 	printf("DFSDM1                   \t %x \t %d bytes \r\n",DFSDM1_BASE     ,0x400177FF  - DFSDM1_BASE); 
  //	printf("DSI Host                 \t %x \t %d bytes \n",DFSDM1_BASE,     ,0x400203FF  - DFSDM1_BASE); 
 	printf("LCD-HOST                 \t %x \t %d bytes \r\n",LTDC_BASE       ,0x40016BFF  - LTDC_BASE);
 	printf("SAI2                 	 \t %x \t %d bytes \r\n",SAI2_BASE       ,0x40015FFF  - SAI2_BASE);
 	printf("SAI1                 	 \t %x \t %d bytes \r\n",SAI1_BASE       ,0x40015BFF  - SAI1_BASE);
-	printf("SPI6                 	 \t %x \t %d bytes \r\n",SPI6_BASE       ,0x400157FF  - SAI6_BASE);
-	printf("SPI5                 	 \t %x \t %d bytes \r\n",SPI5_BASE       ,0x400153FF  - SAI5_BASE);
+	printf("SPI6                 	 \t %x \t %d bytes \r\n",SPI6_BASE       ,0x400157FF  - SPI6_BASE);
+	printf("SPI5                 	 \t %x \t %d bytes \r\n",SPI5_BASE       ,0x400153FF  - SPI5_BASE);
 	printf("TIM11                 	 \t %x \t %d bytes \r\n",TIM11_BASE      ,0x40014BFF  - TIM11_BASE);
 	printf("TIM10                 	 \t %x \t %d bytes \r\n",TIM10_BASE      ,0x400147FF  - TIM10_BASE);
 	printf("TIM9                 	 \t %x \t %d bytes \r\n",TIM9_BASE       ,0x400143FF  - TIM9_BASE);
@@ -63,7 +64,7 @@ g
 	
 }
  
-COMMAND_ENTRY("memmap", "memmap", "Display where different physical hardware peripherals", do_memmap); 
+COMMAND_ENTRY("memmap", "memmap", "Display where different physical hardware peripherals", do_memmap) 
 
 
 
