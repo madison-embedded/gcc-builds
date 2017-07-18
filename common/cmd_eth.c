bb1bc0511f26d0286b7541238113b29714426bd6
vkottler <vkottler@wisc.edu>
2017-07-18 00:42:20 -0500

    stopping for tonight

--- common/cmd_eth.c
+++ common/cmd_eth.c
@@ -56,6 +56,30 @@ inline void eth_init(void) {
     HAL_ETH_Start(&EthHandle);
 }
 
+void eth_print_status(HAL_ETH_StateTypeDef stat) {
+	switch (stat) {
+		case HAL_ETH_STATE_RESET: printf("RESET"); break;
+		case HAL_ETH_STATE_READY: printf("READY"); break;
+		case HAL_ETH_STATE_BUSY: printf("BUSY"); break;
+		case HAL_ETH_STATE_BUSY_RX: printf("BUSY_RX"); break;
+		case HAL_ETH_STATE_BUSY_TX: printf("BUSY_TX"); break;
+		case HAL_ETH_STATE_BUSY_TX_RX: printf("BUSY_TX_RX"); break;
+		case HAL_ETH_STATE_BUSY_WR: printf("BUSY_WR"); break;
+		case HAL_ETH_STATE_BUSY_RD: printf("BUSY_RD"); break;
+		case HAL_ETH_STATE_TIMEOUT: printf("TIMEOUT"); break;
+		case HAL_ETH_STATE_ERROR: printf("ERROR"); break;
+	}
+}
+
+void hal_print_status(HAL_StatusTypeDef stat) {
+	switch (stat) {
+		case HAL_OK: printf("OK"); break;
+		case HAL_ERROR: printf("ERROR"); break;
+		case HAL_BUSY: printf("BUSY"); break;
+		case HAL_TIMEOUT: printf("TIMEOUT"); break;
+	}
+}
+
 command_status do_eth(int argc, char *argv[]) {
 
 	uint16_t phy_reg;
@@ -76,12 +100,30 @@ command_status do_eth(int argc, char *argv[]) {
 		}
 		ret = HAL_ETH_ReadPHYRegister(&EthHandle, phy_reg, &regVal);
 		if (ret != HAL_OK) {
-			printf("Could not read register! (%d)\r\n", ret);
+			printf("Could not read register! ");
+			hal_print_status(ret);
+			printf("\r\n");
 			return FAIL;
 		}
 		printf("register %d value: 0x%lx\r\n", phy_reg, regVal & 0xffff);
 	}
 	else if (!strcmp(argv[1], "init")) eth_init();
+	else if (!strcmp(argv[1], "check")) {
+		ret = HAL_ETH_GetReceivedFrame(&EthHandle);
+		printf("Return status: ");
+		hal_print_status(ret);
+		printf("\r\n");
+	}
+	else if (!strcmp(argv[1], "status")) printf("Link status: %lu\r\n", EthHandle.LinkStatus);
+	else if (!strcmp(argv[1], "state")) {
+		printf("State: ");
+		eth_print_status(EthHandle.State);
+		printf("\r\n");
+	}
+	else if (!strcmp(argv[1], "send") && argc >= 3) {
+		printf("Not yet implemented\r\n");
+		//ret =  HAL_ETH_TransmitFrame(&EthHandle, uint32_t FrameLength);
+	}
 	else return USAGE;
 
 	return SUCCESS;
