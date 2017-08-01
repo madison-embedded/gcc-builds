146332a63a755b6eabe2c8387b649591cbd72a11
Cooper Green <csgreen@xes-inc.com>
2017-08-01 14:14:08 -0500

    NOT TESTED cmd command not in Makefile defaults to 50 percent duty cycle

--- common/cmd_pwm.c
+++ common/cmd_pwm.c
@@ -7,16 +7,19 @@
 
 command_status do_pwm(int argc, char *argv[]) {
 	if (argc < 2) return USAGE;
-	bool pinOnly = false, useAlias = false;
-	char portTemp = argv[1][0];
-	int group;
-	uint8_t pin = 0, i;
-	GPIO_TypeDef * port; 
-	GPIO_MODE mode;
-
-	port = gpio_getGpioPort(portTemp);
+	uint8_t channel = atoi((const char *) argv[1]);
+    uint8_t duty;
+    
+    if (argc > 2)
+	    duty = atoi((const char *) argv[2]);
+    else
+	    duty = 50;
+        
+    TIM_PWM_Init(TIM1, channel, duty);
+    
 
 	return SUCCESS;
+
 }
 COMMAND_ENTRY("pwm", "pwm <port>[num] [duty cycle]", "Perform live initialization of pulse with modulation for specified pins.", do_pwm)
 
