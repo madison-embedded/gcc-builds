0a0cf5cf1f8f38b300bdbf65f354570965807096
Cooper Green <csgreen@xes-inc.com>
2017-08-08 09:59:53 -0500

    I2C blocking read and write command I added the command to read and write it compiles without warnings however has not been tested This would be a bog way to start working on the I2C sensor drivers
    Signed-off-by: Cooper Green <csgreen3@wisc.edu>

--- common/cmd_i2c.c
+++ common/cmd_i2c.c
@@ -9,34 +9,52 @@ extern I2C_HandleTypeDef hi2c;
 
 
 char * getStatus(HAL_StatusTypeDef status){
-	
-	switch (status){
-		case HAL_OK:
-			return "OK";
-		case HAL_ERROR:
-			return "ERROR";
-		case HAL_BUSY:
-			return "BUSY";
-		case HAL_TIMEOUT:
-			return "TIMEOUT";
-		default:
-			return "NOTGOOD";
-	}
-	return "NOTGOOD";
+
+    switch (status){
+        case HAL_OK:
+            return "OK";
+        case HAL_ERROR:
+            return "ERROR";
+        case HAL_BUSY:
+            return "BUSY";
+        case HAL_TIMEOUT:
+            return "TIMEOUT";
+        default:
+            return "NOTGOOD";
+    }
+    return "NOTGOOD";
 }
 
 command_status do_i2c(int argc, char *argv[]) {
-	uint8_t address;
+    uint16_t address;
+    uint8_t *pData;
+    uint16_t MemAdd;
+    uint16_t MemAddSize;
 
-	printf("%s\r\n", getStatus(HAL_I2C_Init(&hi2c)));
+    printf("%s\r\n", getStatus(HAL_I2C_Init(&hi2c)));
 
-	for (address = 0; address < 128; address++){
+    if (argc == 1) {
+        for (address = 0; address < 128; address++)
+            printf("%x: %s\r\n", address, getStatus(HAL_I2C_IsDeviceReady(&hi2c, address<<1, 1, 5000)));
+    }
 
-  		printf("%x: %s\r\n", address, getStatus(HAL_I2C_IsDeviceReady(&hi2c, address<<1, 1, 5000)));
-	
-	}
-	return SUCCESS;
+    if (argc != 6)
+        return USAGE;
 
+    address = atoi((const char *) &argv[2]);
+    MemAdd = atoi((const char *) &argv[3]);
+    MemAddSize = atoi((const char *) &argv[4]);
+    pData = (uint8_t *)atoi((const char *) &argv[5]);
+
+    if (strcmp(argv[1], "write") == 0) {
+        getStatus(HAL_I2C_Mem_Write(&hi2c, address, MemAdd, MemAddSize, pData, sizeof(pData), 500));
+        return SUCCESS;
+    }
+    if (strcmp(argv[1], "read") == 0) {
+        getStatus(HAL_I2C_Mem_Read(&hi2c, address, MemAdd, MemAddSize, pData, sizeof(pData), 500));
+        return SUCCESS;
+    }
+    return USAGE;
 }
-COMMAND_ENTRY("i2c", "i2c <read | write> <address> <offset>", "Perform live interaction with the i2c devices default list all devices", do_i2c)
+COMMAND_ENTRY("i2c", "i2c <read | write> <address> <offset> <Memory Address Size> [value]", "Perform live interaction with the i2c devices default list all devices", do_i2c)
 
