70693090f7eedceabdd6b7b8c007c679016b6cd0
davidguyuchen <davidguyuchen@gmail.com>
2017-08-15 22:34:12 -0500

    temp commit

---
+++ include/HWPressure.h
@@ -0,0 +1,19 @@
+#ifndef _HWPressure__H__
+#define _HWPressure__H__
+
+#include "../include/stm32_i2c.h"
+#include <stdint.h>
+#include <float.h>
+
+
+#define PRESSURE_ADDRESS		0x78				
+#define OUTPUTMAX			14746
+#define OUTPUTMIN			1638
+#define PRESSUREMAX			87.0228
+#define PRESSUREMIN			0
+void HPread(void);
+double HPgetStatus(void);
+double HPgetPressure(void);
+double HPgetTemperature(void);
+
+#endif
