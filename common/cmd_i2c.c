01cbd94ac937fbefae150ab31eb4a089aedad565
Cooper Green <csgreen3@wisc.edu>
2017-08-20 18:48:28 -0500

    Added I2C command improvments
    Signed-off-by: Cooper Green <csgreen3@wisc.edu>

--- common/cmd_i2c.c
+++ common/cmd_i2c.c
@@ -40,7 +40,7 @@ command_status do_i2c(int argc, char *argv[]) {
 		}
 		return SUCCESS;
 	}
-	
+
 	/*  */
 	if (argc < 3 || argc > 6)
 		return USAGE;
@@ -48,46 +48,47 @@ command_status do_i2c(int argc, char *argv[]) {
 	address = strtoul(argv[2], NULL, 16);
 
 	MemAdd = atoi((const char *) argv[3]);
-	
+
 	if (strcmp(argv[1], "write") == 0) {
-	
+
 		if (argc != 5)
 			return USAGE;
-	 	pData= strtoul(argv[4], NULL, 16);
-	 	
-	 	if (HAL_I2C_IsDeviceReady(&hi2c, address<<1, 1, 5000) != HAL_OK){
-	 		printf("0x%x: %s\r\n", address, getStatus(HAL_I2C_IsDeviceReady(&hi2c, address<<1, 1, 500)));
-	 		return USAGE;
-	 	}
-	 	printf("%s\r\n",getStatus(HAL_I2C_Mem_Write(&hi2c, address<<1, MemAdd, 1, &pData, 1, 500)));
-	 	return SUCCESS;
-	}
-	if (strcmp(argv[1], "read") == 0) {
-		if (argc < 5){
-			numBytes = atoi((const char *) argv[3]);
-			
-			printf("%s\r\n", getStatus(HAL_I2C_Master_Receive(&hi2c, address<<1, pBuffer , numBytes, 500)));
-			
-			for (i = 0; i < numBytes; i ++)
-				printf("0x%x\r\n", (pBuffer[i]));
-		
-		
-		return SUCCESS;	
+		pData= strtoul(argv[4], NULL, 16);
 
-		}
-		else{
-	
 		if (HAL_I2C_IsDeviceReady(&hi2c, address<<1, 1, 5000) != HAL_OK){
 			printf("0x%x: %s\r\n", address, getStatus(HAL_I2C_IsDeviceReady(&hi2c, address<<1, 1, 500)));
 			return USAGE;
 		}
-
-		printf("%s\r\n",getStatus(HAL_I2C_Mem_Read(&hi2c, address<<1, MemAdd, 1, &pData, 1, 500)));
-		printf("address: 0x%x Data: 0x%x\r\n", address, pData);
+		printf("%s\r\n",getStatus(HAL_I2C_Mem_Write(&hi2c, address<<1, MemAdd, 1, &pData, 1, 500)));
 		return SUCCESS;
+	}
+	if (strcmp(argv[1], "read") == 0) {
+		if (argc == 4){
+			numBytes = atoi((const char *) argv[3]);
+
+			printf("%s\r\n", getStatus(HAL_I2C_Master_Receive(&hi2c, address<<1, pBuffer , numBytes, 500)));
+
+			for (i = 0; i < numBytes; i ++)
+				printf("0x%x\r\n", (pBuffer[i]));
+
+
+			return SUCCESS;	
+
+		}
+		else if (argc == 5){
+
+			if (HAL_I2C_IsDeviceReady(&hi2c, address<<1, 1, 5000) != HAL_OK){
+				printf("0x%x: %s\r\n", address, getStatus(HAL_I2C_IsDeviceReady(&hi2c, address<<1, 1, 500)));
+				return USAGE;
+			}
+
+			printf("%s\r\n",getStatus(HAL_I2C_Mem_Read(&hi2c, address<<1, MemAdd, 1, &pData, 1, 500)));
+			printf("address: 0x%x Data: 0x%x\r\n", address, pData);
+
+			return SUCCESS;
 		}
 	}
 	return USAGE;
 }
-COMMAND_ENTRY("i2c", "i2c <read | write> <address HEX> <offset> [value HEX]", "Perform live interaction with the i2c devices default list all devices", do_i2c)
+COMMAND_ENTRY("i2c", "i2c <read | write> <address HEX> <offset | num bytes> [value HEX]", "Perform live interaction with the i2c devices default list all devices", do_i2c)
 
