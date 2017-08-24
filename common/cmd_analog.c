b2671bf780f4a7f5fe9c835ddf972a50b16e93ec
Cooper Green <csgreen3@wisc.edu>
2017-08-23 22:03:59 -0700

    Added ar GROUP for Pressure
    Signed-off-by: Cooper Green <csgreen3@wisc.edu>

--- common/cmd_analog.c
+++ common/cmd_analog.c
@@ -9,34 +9,51 @@
 
 uint16_t get_performanceITI(uint16_t(*func)(ADC_TypeDef *, uint8_t), ADC_TypeDef * adc, uint8_t pin);
 
+extern int isGroup(char * group);
+
 command_status do_ar(int argc, char *argv[]) {
 	char portTemp;
-	uint8_t pin = 0, i;
+	uint8_t pin = 0, i, ii;
 	GPIO_TypeDef * port;
 	GPIO gpio;
+	int group;
 
 
 	if (argc == 1){
 		for (i = 0; i < NUM_ADC; i++){
 			if (getGpioAlias((GPIO_TypeDef **)&(ADC_LUT[i].port), (uint8_t *)&(ADC_LUT[i].pin), &gpio))
-			printf("%s: %d\r\n", gpio.name, analogRead(ADC_LUT[i].adc, ADC_LUT[i].channel));
+				printf("%s: %d\r\n", gpio.name, analogRead(ADC_LUT[i].adc, ADC_LUT[i].channel));
 			else
-			printf("Result: %d\r\n", analogRead(ADC_LUT[i].adc, ADC_LUT[i].channel));
+				printf("Result: %d\r\n", analogRead(ADC_LUT[i].adc, ADC_LUT[i].channel));
 		}
 		return SUCCESS;
 	}
 
 	portTemp = argv[1][0];
 
+	group = isGroup(argv[1]);
+	if (group != -1) {
+		for (i=0; i<NUM_GPIO_ALIAS; i++) {
+			if (group & GPIO_TABLE[i].group) {
+				for (ii = 0; ii < NUM_ADC; ii++){
+					if (ADC_LUT[ii].port == GPIO_TABLE[i].port && ADC_LUT[ii].pin == GPIO_TABLE[i].pin){
+						printf("%s: %d\r\n", GPIO_TABLE[i].name, analogRead(ADC_LUT[ii].adc, ADC_LUT[ii].channel));
+					}
+				}
+			}
+		}
+		return SUCCESS;
+	}
+
 	if (hasGpioAlias(&port, &pin, argv[1])){
 		for (i = 0; i < NUM_ADC; i++){
 			if (ADC_LUT[i].port == port && ADC_LUT[i].pin == pin){
 				if (argc>2) {
-				if (strcmp(argv[2], "perf") == 0)
-					get_performanceITI(analogRead, ADC_LUT[i].adc, ADC_LUT[i].channel);
+					if (strcmp(argv[2], "perf") == 0)
+						get_performanceITI(analogRead, ADC_LUT[i].adc, ADC_LUT[i].channel);
 				}
 				printf("Result: %d\r\n", analogRead(ADC_LUT[i].adc, ADC_LUT[i].channel));
-				
+
 				return SUCCESS;
 			}
 		}
